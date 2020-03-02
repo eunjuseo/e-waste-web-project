3d3c9f289d7156195c679a7d8f0b3c88c9dac529
eunju <tjdmswn9604@gmail.com>
2020-03-01 21:15:00 -0800

    e-waste project outline

---
+++ frontend/src/components/Feed/Post/Post.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Button from '../../Button/Button';
+import './Post.css';
+
+const post = props => (
+  <article className="post">
+    <header className="post__header">
+      <h1 className="post__title">{props.title}</h1>
+    </header>
+    <p className="post__content">{props.content}</p>
+    {/* <div className="post__actions">
+      <Button mode="flat" link={props.id}>
+        View
+      </Button>
+      <Button mode="flat" onClick={props.onStartEdit}>
+        Edit
+      </Button>
+      <Button mode="flat" design="danger" onClick={props.onDelete}>
+        Delete
+      </Button>
+    </div> */}
+  </article>
+);
+
+export default post;
---
+++ frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import Image from '../../../components/Image/Image';
+import './SinglePost.css';
+
+class SinglePost extends Component {
+  state = {
+    title: '',
+    //author: '',
+    date: '',
+    image: '',
+    content: ''
+  };
+
+  componentDidMount() {
+    const postId = this.props.match.params.postId;
+    fetch('http://localhost:8080/feed/post/' + postId)
+      .then(res => {
+        if (res.status !== 200) {
+          throw new Error('Failed to fetch status');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        this.setState({
+          title: resData.post.title,
+          //author: resData.post.creator.name,
+          image: 'http://localhost:8080/' + resData.post.imageUrl,
+          date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
+          content: resData.post.content
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <section className="single-post">
+        <h1>{this.state.title}</h1>
+        <p>{this.state.content}</p>
+      </section>
+    );
+  }
+}
+
+export default SinglePost;
